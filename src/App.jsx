3c8b3652116b140169ab4244a0301b035b2e0bae
azevedosvg <azevedosv.gabriel2@gmail.com>
2026-05-11 22:57:12 -0300

    feat: global css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Topbar from "./components/Topbar";
 import StatsRow from "./components/StatsRow";
 import RecentDeliveries from "./components/RecentDeliveries";
 import ActivityFeed from "./components/ActivityFeed";
-
 export default function App() {
   return (
     <div className="layout">
